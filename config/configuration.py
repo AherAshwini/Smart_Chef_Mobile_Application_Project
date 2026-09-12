c62b0daf3f6fe7602bbdb696ac582d4b9a331748
AherAshwini <aherashwini25@gmail.com>
2026-09-12 16:59:39 +0530

    Update Dataset Path

--- config/configuration.py
+++ config/configuration.py
@@ -3,4 +3,13 @@ import os
 
 
 ### Path of downloaded data.
-DATASET_PATH = Path("C:\\Users\\ahera\\Downloads\\Smart_Chef_Mobile_App_Fruits-360_Dataset")
+DATASET_PATH = Path("C:\Users\ahera\Downloads\Smart_Chef_Mobile_App_Fruits-360_Dataset\fruits-360_100x100\fruits-360")
+
+### Number of randomly selected classes.
+NUM_CLASSES = 20
+
+### Number of randomly selected images per class.
+IMAGES_PER_CLASS = 500
+
+### Total Number of images.
+TOTAL_IMAGES = 10000
